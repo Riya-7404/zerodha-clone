a1f691bbd81f2b36b6b348ca120d345c65500fcc
Riya Gaikwad <gaikwadriya138@gmail.com>
2025-09-01 22:09:02 +0530

    Update Hero.js

--- frontend/src/landing_page/home/Hero.js
+++ frontend/src/landing_page/home/Hero.js
@@ -2,20 +2,20 @@ import React from 'react';
 
 function Hero() {
     return ( 
-    <div className='container p-5'> 
-       <div className='row text-center'>
-           <img src='media/images/HomeHero.png'  alt="Hero Image" className="mb-5" />
-           <h1 ClassName="mt-5">Invest in everything</h1>
-           <p>Online platform to invest in stocks, derivatives,mutual funds, and more</p>
-           <button 
-                className="btn btn-primary fs-5 mb-5 px-4 py-1" 
-                style={{ width: "20%", margin: "0 auto" }}
-               >
-             Signup Now
-            </button>
+        <div className='container p-5'> 
+            <div className='row text-center'>
+                <img src='/media/images/HomeHero.png' alt="Hero Image" className="mb-5" />
+                <h1 className="mt-5">Invest in everything</h1>
+                <p>Online platform to invest in stocks, derivatives, mutual funds, and more</p>
+                <button 
+                    className="btn btn-primary fs-5 mb-5 px-4 py-1" 
+                    style={{ width: "20%", margin: "0 auto" }}
+                >
+                    Signup Now
+                </button>
+            </div>
         </div>
-    </div>
-);
+    );
 }
 
 export default Hero;
